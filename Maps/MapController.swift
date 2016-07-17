8d42e40d825e823e14fee79f149812bac71d7299
Rudson Lima <rudsonlive@gmail.com>
2016-07-17 16:00:51 -0300

    Creating the project

---
+++ Maps/MapController.swift
@@ -0,0 +1,65 @@
+//
+//  MapController.swift
+//  Maps
+//
+//  Created by Rudson Lima on 7/16/16.
+//  Copyright © 2016 Rudson Lima. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureLocationManager();
+    }
+    
+    func configureLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Seu não tiver permissão, fazer o pedido de acesso a localização ao usuário
+        if CLLocationManager.authorizationStatus() != .AuthorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        mapView.showsUserLocation = true
+        locationManager.startUpdatingLocation()
+    }
+    
+    func configureMapView(coordinate: CLLocationCoordinate2D, region: MKCoordinateRegion){
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "Fortaleza/CE"
+        annotation.subtitle = "Brasil"
+        annotation.coordinate = coordinate
+        
+        mapView.addAnnotation(annotation)
+        mapView.setRegion(region, animated: true)
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        
+        let location = locations.last
+        let center = CLLocationCoordinate2DMake(location!.coordinate.latitude, location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+        
+        configureMapView(location!.coordinate, region: region)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError){
+        print("Errors: " + error.localizedDescription)
+    }
+}
+
