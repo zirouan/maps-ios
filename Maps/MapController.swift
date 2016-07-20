6c71b15f77fee27bdaf7ba536282a7a165152702
Rudson Lima <rudsonlive@gmail.com>
2016-07-20 12:32:22 -0300

    Custom map pin

--- Maps/MapController.swift
+++ Maps/MapController.swift
@@ -49,11 +49,31 @@ class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDeleg
         locationManager.stopUpdatingLocation()
     }
     
+    // MKMapViewDelegate - Custom map pin
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        
+        let annotationReuseId = "Place"
+        
+        var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(annotationReuseId)
+        
+        if anView == nil {
+            anView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationReuseId)
+        } else {
+            anView!.annotation = annotation
+        }
+        
+        anView!.image = UIImage(named: "MapPin")
+        anView!.backgroundColor = UIColor.clearColor()
+        anView!.canShowCallout = true
+        return anView
+    }
+    
+    // CLLocationManagerDelegate
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         
         let location = locations.last
         let center = CLLocationCoordinate2DMake(location!.coordinate.latitude, location!.coordinate.longitude)
-        
+                
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
         
         configureMapView(location!.coordinate, region: region)
